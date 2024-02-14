f5e519b6297912d7430dd6cf36f93ab0479408b4
Lpinheirocl <lucas.pinheiro1014@gmail.com>
2024-02-14 15:47:24 -0300

    tabelas e lista

---
+++ database/cliente.py
@@ -0,0 +1,7 @@
+CLIENTES = [
+    {"id":1, "nome": "Lucas", "email": "teste@teste.com.br"},
+    {"id":2, "nome": "Duque", "email": "teste@teste.com.br"},
+    {"id":3, "nome": "Lu", "email": "teste@teste.com.br"},
+    {"id":4, "nome": "cas", "email": "teste@teste.com.br"},
+    {"id":5, "nome": "Lus", "email": "teste@teste.com.br"},
+]
--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 from flask import Flask, url_for, render_template
+from routes.home import home_route
+from routes.cliente import cliente_route
 
 # Inicializacao
 app = Flask(__name__)
 
 # rotas
-@app.route("/")
-def ola_mundo():
-    titulo = "Gestão de Usuários"
-    usuarios = [
-        {"nome": "Lucas", "membro_ativo": True},
-        {"nome": "Duque", "membro_ativo": False},
-    ]
-    return render_template('index.html', titulo=titulo, usuarios=usuarios)
+app.register_blueprint(home_route)
+app.register_blueprint(cliente_route, url_prefix='/clientes')
 
-@app.route("/sobre")
-def pagina_sobre():
-    return """
-        <b>Progamodor Python</b>: Vamos la
-        """
-#execucao
+
 app.run(debug=True)
